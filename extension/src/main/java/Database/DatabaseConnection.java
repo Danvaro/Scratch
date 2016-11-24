6895e3bcdcc01ad10679d0e0ea8bf74a3586a4f3
Danvari <lampkap@gmail.com>
2016-11-24 23:20:21 +0100

    Added guest login for registration

---
+++ extension/src/main/java/Database/DatabaseConnection.java
@@ -0,0 +1,42 @@
+package Database;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.smartfoxserver.v2.extensions.ISFSExtension;
+import com.smartfoxserver.v2.extensions.SFSExtension;
+import org.bson.Document;
+
+public class DatabaseConnection extends SFSExtension implements ISFSExtension {
+
+    public MongoClient mongoClient;
+    public MongoDatabase scratch;
+    public static MongoCollection<Document> users;
+
+    @Override
+    public void init() {
+
+    }
+
+    public DatabaseConnection(String host, int port, String database, String collection) {
+        try {
+            mongoClient = new MongoClient(host, port);
+            scratch = mongoClient.getDatabase(database);
+            DatabaseConnection.users = scratch.getCollection(collection);
+
+        } catch (Exception e) {
+            handleError(e);
+        }
+    }
+
+    private void handleError(Exception e) {
+        trace(e);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        mongoClient.close();
+        trace("Database connection -- stopped");
+    }
+}
---
+++ extension/src/main/java/MainExtension.java
@@ -0,0 +1,25 @@
+import Authentication.LoginHandler;
+import Database.DatabaseConnection;
+import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.extensions.ISFSExtension;
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+public class MainExtension extends SFSExtension implements ISFSExtension {
+
+    @Override
+    public void init() {
+        this.addRequestHandlers();
+        this.databaseConnection();
+    }
+
+    private void addRequestHandlers() {
+        addEventHandler(SFSEventType.USER_LOGIN, LoginHandler.class);
+
+    }
+
+    private void databaseConnection() {
+        DatabaseConnection dataCon = new DatabaseConnection("localhost", 27017, "scratch", "users");
+    }
+
+
+}
